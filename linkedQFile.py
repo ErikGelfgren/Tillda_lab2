8160eb218787939f0c1eda1deb3aa42547ef83c6
ErikGelfgren <99972480+ErikGelfgren@users.noreply.github.com>
2023-09-08 11:05:33 +0200

    Update linkedQFile.py

--- linkedQFile.py
+++ linkedQFile.py
@@ -1,27 +1,31 @@
 class LinkedQ():
     def __init__(self):
-        self.first=None
-        self.last=None
+        #Define first and last node in linked list
+        self._first=None
+        self._last=None
 
     def isEmpty(self):
-        return self.first==None
+        #Checks if First node is empty and returns True or False
+        return self._first==None
 
     def enqueue(self,value):
+        #Places new node in end of list and links it with last node
         new_val=Node(value)
-        if self.first is None:
-            self.first=new_val
-            self.last=self.first
+        if self._first is None:
+            self._first=new_val
+            self._last=self._first
         else:
-            self.last.next=new_val
-            self.last=new_val
+            self._last.next=new_val
+            self._last=new_val
 
     def dequeue(self):
-        value=self.first.value
-        self.first=self.first.next
+        #Stores value of first node and removes it from linked list
+        value=self._first.value
+        self._first=self._first.next
         return value
 
 class Node():
+    #Creates node with stored value and link to next node
     def __init__(self,value,next=None):
         self.value=value
         self.next=next
-    
